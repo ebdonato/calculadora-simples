2262d705d43fc4f42a1b6ed91e200dac7665ed67
ebdonato <63134122+ebdonato@users.noreply.github.com>
2020-11-14 11:12:32 -0300

    Possibilidade PWA e Deploy Firebase

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -3,3 +3,18 @@
  * is picked up by the build system ONLY if
  * quasar.conf > pwa > workboxPluginMode is set to "InjectManifest"
  */
+
+//dependências - precaching
+import { precacheAndRoute } from 'workbox-precaching'
+
+//dependências - caching
+import { registerRoute } from 'workbox-routing'
+import { StaleWhileRevalidate } from 'workbox-strategies'
+
+//configurações - precaching
+precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+    ({ url }) => url.href.startsWith('http'),
+    new StaleWhileRevalidate()
+)
